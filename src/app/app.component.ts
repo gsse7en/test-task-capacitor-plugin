58cad64b21bad2a2b257806dc45598ccaf3452f3
gsse7en <latyshev.fedor@gmail.com>
2023-11-15 23:02:50 +0200

    add button with snackbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { MatButtonModule } from '@angular/material/button';
+import {
+  MatSnackBar,
+} from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, MatButtonModule],
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'test-task-capacitor-plugin';
+  message = 'Hello World';
+  constructor(private _snackbar: MatSnackBar) {}
+  sendMessage(text: string) {
+    this._snackbar.open(text);
+  }
 }
